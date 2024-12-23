04432e9fe6d2c41b4f86c3cab1497e6a749854c8
sayoriqwq <2531600563@qq.com>
2024-12-23 14:46:12 +0800

    feat: clerk

--- src/layout/header/operations.tsx
+++ src/layout/header/operations.tsx
@@ -1,10 +1,24 @@
 import { ModeToggle } from '@/components/common/mode-toggle'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { LogIn } from 'lucide-react'
 import { NavItem } from './nav-item'
 
 export function Operations() {
   return (
-    <NavItem>
-      <ModeToggle />
-    </NavItem>
+    <div className="flex">
+      <NavItem>
+        <ModeToggle />
+      </NavItem>
+      <NavItem>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <LogIn className="size-4" />
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </NavItem>
+    </div>
   )
 }
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import type { PropsWithChildren } from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
 import { FontProvider } from './font-provider'
 import { LenisProvider } from './lenis-provider'
 import { SWRProvider } from './swr-provider'
@@ -17,7 +18,9 @@ export function Providers({ children }: PropsWithChildren) {
       <LenisProvider>
         <FontProvider>
           <SWRProvider>
-            {children}
+            <ClerkProvider>
+              {children}
+            </ClerkProvider>
           </SWRProvider>
         </FontProvider>
       </LenisProvider>
