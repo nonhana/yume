6e80f87dd2e0a6824c91748dda2a5ec2a24a2ea5
sayoriqwq <2531600563@qq.com>
2025-02-12 21:22:13 +0800

    fix: eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import antfu from '@antfu/eslint-config'
-import tailwind from 'eslint-plugin-tailwindcss'
+// import tailwind from 'eslint-plugin-tailwindcss'
 
 export default antfu(
   {
@@ -8,30 +8,30 @@ export default antfu(
       markdown: 'prettier',
     },
   },
-  ...tailwind.configs['flat/recommended'],
-  {
-    settings: {
-      tailwindcss: {
-        // These are the default values but feel free to customize
-        callees: ['classnames', 'clsx', 'ctl'],
-        config: 'tailwind.config.js', // returned from `loadConfig()` utility if not provided
-        cssFiles: [
-          '**/*.css',
-          '!**/node_modules',
-          '!**/.*',
-          '!**/dist',
-          '!**/build',
-        ],
-        cssFilesRefreshRate: 5_000,
-        removeDuplicates: true,
-        skipClassAttribute: false,
-        whitelist: [],
-        tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
-        classRegex: '^class(Name)?$', // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
-      },
-    },
-    rules: {
-      'tailwindcss/no-custom-classname': 'off', // 禁用 no-custom-classname 规则
-    },
-  },
+//   ...tailwind.configs['flat/recommended'],
+//   {
+//     settings: {
+//       tailwindcss: {
+//         // These are the default values but feel free to customize
+//         callees: ['classnames', 'clsx', 'ctl'],
+//         // config: 'tailwind.config.js', // returned from `loadConfig()` utility if not provided
+//         cssFiles: [
+//           '**/*.css',
+//           '!**/node_modules',
+//           '!**/.*',
+//           '!**/dist',
+//           '!**/build',
+//         ],
+//         cssFilesRefreshRate: 5_000,
+//         removeDuplicates: true,
+//         skipClassAttribute: false,
+//         whitelist: [],
+//         tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
+//         classRegex: '^class(Name)?$', // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
+//       },
+//     },
+//     rules: {
+//       'tailwindcss/no-custom-classname': 'off', // 禁用 no-custom-classname 规则
+//     },
+//   },
 )
